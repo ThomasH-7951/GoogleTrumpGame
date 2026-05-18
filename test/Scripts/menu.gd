993479c44cf09b0a2e5c65afdf7e233492d1997d
ThomasHenriques <henriquesthomas3@gmail.com>
2026-05-18 21:44:42 +0200

    ajout de la pause

--- test/Scripts/menu.gd
+++ test/Scripts/menu.gd
@@ -1,15 +1,5 @@
 extends Control
-@onready var firstlevel = preload("res://Scenes/niveautest.tscn") 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@onready var firstlevel = preload("res://Scenes/Level.tscn") 
 
 func _on_start_btn_button_down() -> void:
 	get_tree().change_scene_to_packed(firstlevel)
