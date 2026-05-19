eb1b49e76cd217a89b3937bba7b71408cea80c24
ThomasHenriques <henriquesthomas3@gmail.com>
2026-05-19 15:26:37 +0200

    temp commit

--- GoogleTrumpGame/Scripts/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-@onready var firstlevel = preload("res://Scenes/Level.tscn") 
-
-func _on_start_btn_button_down() -> void:
-	get_tree().change_scene_to_packed(firstlevel)
-
-
-func _on_quit_btn_button_down() -> void:
-	get_tree().quit()
