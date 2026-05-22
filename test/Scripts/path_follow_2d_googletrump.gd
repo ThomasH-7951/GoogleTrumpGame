2bad4db9911bddf01c6868aa36974152991c13e3
Creepermanolo <141935544+Creepermanolo@users.noreply.github.com>
2026-05-22 14:17:52 +0200

    delete all

--- test/Scripts/path_follow_2d_googletrump.gd
+++
@@ -1,8 +0,0 @@
-extends PathFollow2D
-
-@export var speed := 500.0
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	progress += speed * delta
-	$Trumpsprite.play()
