d797406ca07fb1b47ad574b59a107cffeb76cb28
ThomasHenriques <henriquesthomas3@gmail.com>
2026-05-28 10:49:51 +0200

    ajout de space pour start et menu

---
+++ Menus/Start_Menu/menu.gd
@@ -0,0 +1,21 @@
+extends Control
+@onready var firstlevel = preload("res://Jeu/Level.tscn") 
+
+func _ready() -> void:
+	get_tree().paused = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("Space"):
+		_on_start_btn_button_down()
+		
+func _on_start_btn_button_down() -> void:
+	get_tree().change_scene_to_packed(firstlevel)
+
+
+
+func _on_quit_btn_button_down() -> void:
+	get_tree().quit()
+#func _process(delta: float) -> void:
+	#var randop = randf()
+	#print(randop)
+	#$TextureRect.modulate = Color(randop,randop,randop)
